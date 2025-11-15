7565262be01e4036302b0f7067d834a7502dc9a9
lberganton <lucasbberganton@gmail.com>
2025-11-15 12:34:38 -0300

    feat: define the basic tty interface

---
+++ kernel/include/tty.h
@@ -0,0 +1,22 @@
+#ifndef TTY_H
+#define TTY_H
+
+int tty_initialize(void);
+int tty_clear(void);
+
+int tty_maxy(void);
+int tty_maxx(void);
+
+int tty_gety(void);
+int tty_getx(void);
+
+int tty_sety(int y);
+int tty_setx(int x);
+
+int tty_getcrlf(void);
+int tty_setcrlf(int status);
+
+int tty_putchar(int ch);
+int tty_printf(const char *format, ...);
+
+#endif
