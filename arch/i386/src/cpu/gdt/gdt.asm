a6b30d192d90c78aadc222188ec56d920d0388d9
lberganton <lucasbberganton@gmail.com>
2025-11-18 03:40:33 -0300

    refactor: little refactor in kernel gdt source code

--- arch/i386/src/cpu/gdt/gdt.asm
+++ arch/i386/src/cpu/gdt/gdt.asm
@@ -1,19 +1,22 @@
+%define GDT_SELECTOR_KERNEL_CODE 0x08
+%define GDT_SELECTOR_KERNEL_DATA 0x10
+
 section .text
 
 global gdt_initialize
 gdt_initialize:
   lgdt [GDT]
 
-  mov ax, (GDT.kernel_data - GDT.begin)
+  mov ax, GDT_SELECTOR_KERNEL_DATA
   mov ds, ax
   mov es, ax
   mov fs, ax
   mov gs, ax
   mov ss, ax
 
-  jmp (GDT.kernel_code - GDT.begin):.ret
+  jmp GDT_SELECTOR_KERNEL_CODE:.flush
 
-.ret:
+.flush:
   ret
 
 section .data
@@ -22,7 +25,7 @@ GDT:
   dw .end - .begin - 1
   dd .begin
   .begin:
-    .null: db 0x00, 0x00, 0x00, 0x00, 0x00, 0b00000000, 0b00000000, 0x00
+    .null:        db 0x00, 0x00, 0x00, 0x00, 0x00, 0b00000000, 0b00000000, 0x00
     .kernel_code: db 0xff, 0xff, 0x00, 0x00, 0x00, 0b10011010, 0b11001111, 0x00
     .kernel_data: db 0xff, 0xff, 0x00, 0x00, 0x00, 0b10010010, 0b11001111, 0x00
 GDT.end:
