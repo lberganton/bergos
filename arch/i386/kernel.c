926a12cd513d540fb223bfbd765e7708cf4302e1
lberganton <lucasbberganton@gmail.com>
2025-11-20 02:41:08 -0300

    feat: add kernel_panic()

--- arch/i386/kernel.c
+++ arch/i386/kernel.c
@@ -2,10 +2,17 @@
 #include "gdt.h"
 #include "i386.h"
 #include "idt.h"
+#include "tty.h"
 
 void kernel_initialize(void) {
   gdt_initialize();
   idt_initialize();
+}
+
+void kernel_panic(int error) {
+  tty_initialize();
+  tty_printf("Kernel Panic! error: 0x%x", error);
+  kernel_halt();
 }
 
 void kernel_halt(void) {
--- kernel/include/kernel.h
+++ kernel/include/kernel.h
@@ -2,6 +2,7 @@
 #define KERNEL_H
 
 void kernel_initialize(void);
+void kernel_panic(int error);
 void kernel_halt(void);
 
 #endif
