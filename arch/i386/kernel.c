a8cf1804654227a270d01d906f349a4ae3083fde
lberganton <lucasbberganton@gmail.com>
2025-11-27 02:01:50 -0300

    refactor: changes kernel_panic()

--- arch/i386/kernel.c
+++ arch/i386/kernel.c
@@ -3,15 +3,22 @@
 #include "i386.h"
 #include "idt.h"
 #include "tty.h"
+#include <stddef.h>
 
 void kernel_initialize(void) {
   gdt_initialize();
   idt_initialize();
 }
 
-void kernel_panic(int error) {
+void kernel_panic(const char *msg) {
   tty_initialize();
-  tty_printf("Kernel Panic! error: 0x%x", error);
+  tty_printf("kernel panic");
+  if (msg == NULL) {
+    tty_printf("!");
+  }
+  else {
+    tty_printf(": %s", msg);
+  }
   kernel_halt();
 }
 
--- kernel/include/kernel.h
+++ kernel/include/kernel.h
@@ -2,7 +2,7 @@
 #define KERNEL_H
 
 void kernel_initialize(void);
-void kernel_panic(int error);
+void kernel_panic(const char *msg);
 void kernel_halt(void);
 
 #endif
