231a92cafd4b109539cf5d4d1360e95e05c6c0df
lberganton <lucasbberganton@gmail.com>
2025-11-28 00:53:24 -0300

    fix: interrupt_initialize() call gdt_initialize() instead of idt_initialize()

--- arch/i386/interrupt.c
+++ arch/i386/interrupt.c
@@ -1,14 +1,14 @@
 #include "interrupt.h"
-#include "gdt.h"
+#include "idt.h"
 #include "i386.h"
 
 void interrupt_initialize(void) {
-  gdt_initialize();
+  idt_initialize();
   i386_cli();
 }
 
 void interrupt_enable(void) {
-    i386_sti();
+  i386_sti();
 }
 
 void interrupt_disable(void) {
